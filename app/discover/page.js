6cd5d51e7ade8b8ac9ea51779b61b09dca4dd954
tharindu432 <tharinduchaturanga432@gmail.com>
2025-05-31 17:44:00 +0530

    fixing th netlify problem

---
+++ app/discover/page.js
@@ -0,0 +1,10 @@
+export const dynamic = "force-dynamic";
+
+export default function DiscoverPage() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Discover Page</h1>
+      <p>This is a dynamic page.</p>
+    </div>
+  );
+}
